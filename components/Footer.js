721e5839abd0f673a4c22f12bc0548cf573f1143
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2020-12-21 20:45:42 +0000

    milk test

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,11 @@
 import styles from './Footer.module.css'
-
+const milk = "test"
+const hrt = "https://form.questionscout.com/5fceb6cba4404526daf7b364?tn="+milk+"&tp="
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-      <a id="jximtydqlledrm-btn" href="https://form.questionscout.com/5fceb6cba4404526daf7b364?tn=&tp=" target="_blank">Open Form</a>
+      <a id="jximtydqlledrm-btn" href={hrt} target="_blank">Open Form</a>
       </footer>
     </>
   )

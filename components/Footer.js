d7484dd9c690d51d1ce5ed5af6315d9c3e299468
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2020-12-21 20:40:00 +0000

    Qtest

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+      <a id="jximtydqlledrm-btn" href="https://form.questionscout.com/5fceb6cba4404526daf7b364?tn=&tp=" target="_blank">Open Form</a>
       </footer>
     </>
   )
